c1361d64cdfdf5db6e451efac41b2ce02e3442e3
robinmihkelson <rmihkelson06@gmail.com>
2026-03-17 15:51:58 +0200

    class work

--- tests/books.spec.js
+++ tests/books.spec.js
@@ -1,12 +1,15 @@
 import { test, expect } from "@playwright/test";
 import { studentTest, BOOKS } from "./helpers.js";
 
-studentTest.fixme("books page loads", async ({ page }) => {
-  // TODO
+studentTest("books page loads", async ({ page }) => {
+    await page.goto("/books");
+    await expect(page.getByTestId("books-list")).toBeVisible();
 });
 
-studentTest.fixme("all 10 seeded books are displayed", async ({ page }) => {
-  // TODO
+studentTest("all 10 seeded books are displayed", async ({ page }) => {
+    await page.goto("/books");
+    const cards = page.getByTestId("books-list").locator("[data-testid^='book-card-']");
+    await expect(cards).toHaveCount(10)
 });
 
 studentTest.fixme("each book shows title and author", async ({ page }) => {
